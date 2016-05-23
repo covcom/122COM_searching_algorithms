18f1b5bf6bcd7520f9006194d519187bc64ee71c
dscroft <dscroft@gmail.com>
2016-05-23 15:27:14 +0100

    Auto commit

---
+++ lab_duplicates.py
@@ -0,0 +1,20 @@
+import sys
+
+def duplicates( values ):
+    # COMPLETE ME
+
+    return False
+
+def main():
+    # === print user instructions ======
+    print( "Enter a series of numbers, one at a time." )
+
+    # === get user input ======
+    inputs = []
+    # COMPLETE ME
+
+    # === confirm to the user what they entered ======
+    print( "You entered :", ', '.join( inputs ) )
+
+if __name__ == '__main__':
+    sys.exit(main())
